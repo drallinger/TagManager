b951fd59d05d4eddddc14bd04f864e9551932fa3
Davin Allinger <drallinger2@gmail.com>
2024-09-13 16:15:18 -0400

    Added toString method to Tag

--- src/main/java/com/drallinger/tagmanager/Tag.java
+++ src/main/java/com/drallinger/tagmanager/Tag.java
@@ -1,3 +1,8 @@
 package com.drallinger.tagmanager;
 
-public record Tag(int ID, String name) {}
+public record Tag(int ID, String name) {
+    @Override
+    public String toString() {
+        return name;
+    }
+}
